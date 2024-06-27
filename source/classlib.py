c06978c86ce22b16f77e0d75ca277448d921cd3b
su2u4 <Joey23.su@gmail.com>
2024-06-28 01:50:07 +0800

    other success

--- source/classlib.py
+++ source/classlib.py
@@ -1,11 +1,71 @@
+import os.path
+import json5
+
+__all__ = ["init", "Bag", "Player", "TEXT", "DATA"]
+TEXT: dict = {}
+DATA: dict = {}
+
+
+def init(root: str, language: str = "en") -> None:
+    global TEXT, DATA
+    path = os.path.join(root, "data", f"data.json5")
+    with open(path, "r") as f:
+        DATA = json5.load(f)
+    path = os.path.join(root, "data", f"{language}.json5")
+    if os.path.isfile(path):
+        with open(path, "r") as f:
+            TEXT = json5.load(f)
+    else:
+        path = os.path.join(root, "data", f"{DATA["language_list"][0]}.json5")
+        with open(path, "r") as f:
+            TEXT = json5.load(f)
+
+
+class Bag(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __getitem__(self, key: str) -> int:
+        if key in self:
+            return super().__getitem__(key)
+        else:
+            super().__setitem__(key, 0)
+            return 0
+
+    def __setitem__(self, key: str, value: int) -> None:
+        if value > 0:
+            return super().__setitem__(key, value)
+        elif value == 0:
+            del self[key]
+        else:
+            raise RuntimeError(f"Item {key} quantity is negative.")
+
+    def show(self) -> None:
+        print("[名稱    ][數量]")
+        for k, v in self.items():
+            print(f"[{TEXT[k]:<6}][{v:<4}]")
+
+
 class Player:
     def __init__(self, name: str) -> None:
         self.name = name
         self.money = 0
-        self.bag = {}
+        self.bag = Bag()
         self.corral_n = 1
         self.farmland_n = 5
         self.day = 1
+        self.stamina = 10
+        self.stamina_max = 20
 
-    def show_bag(self) -> None:
-        print(self.bag)
+    def save_archive(self, root: str) -> bool:
+        name = input("存檔名稱(輸入-1取消):")
+        if name == "-1":
+            return False
+        path = os.path.join(root, "archive", name) + ".json5"
+        if os.path.isfile(path):
+            option = input("存檔已存在，是否要覆蓋(Yes/No):")
+            if option != "Yes" and option != "yes" and option != "y" and option != "Y":
+                return False
+        with open(path, "+w") as f:
+            f.write(json5.dumps(self.__dict__))
+        return True
--- source/main.py
+++ source/main.py
@@ -1,4 +1,5 @@
-import os.path, json5
+import os.path
+import json5
 from classlib import *
 
 
@@ -47,21 +48,33 @@ def start() -> None:
 
 def main(player: Player) -> None:
     def other() -> None:
-        option = input("[1.設定][2.存檔][3.離開遊戲][4.繼續遊戲]:")
+        option = input("[1.更改語言][2.存檔][3.離開遊戲][4.繼續遊戲]:")
         if option == "1":
-            pass
+            while True:
+                print("語言列表:")
+                for i in DATA["language_list"]:
+                    print(i)
+                language = input("輸入語言id:")
+                if language in DATA["language_list"]:
+                    init(root, language)
+                    break
+                print("輸入錯誤")
         elif option == "2":
-            pass
+            if player.save_archive(root):
+                print("存檔成功")
+            else:
+                print("存檔失敗")
+                other()
         elif option == "3":
             f = input("你可能還沒存檔，你確定要離開遊戲嗎?(Yes/No):")
-            if f == "Yes":
+            if f == "Yes" or f == "yes" or f == "y" or f == "Y":
                 print("遊戲關閉")
                 exit()
             else:
                 print("取消")
         elif option != "4":
             print("輸入錯誤")
-            return other()
+            other()
 
     print("遊戲開始")
     while True:
@@ -87,4 +100,5 @@ if __name__ == "__main__":
     root = os.path.abspath(".")
     if root.endswith("source"):
         root = root[:-7]
+    init(root, "zh-tw")
     start()
