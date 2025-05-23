3c41e935c0cfb85dbea83508a21a53be20b7da66
su2u4 <Joey23.su@gmail.com>
2025-05-23 22:54:11 +0800

    rename: update.py -> other.py; feat: move limit_range function to other.py

--- src/other.py
+++ src/other.py
@@ -1,9 +1,45 @@
-from random import randint as ri
 from random import choices
+from random import randint as ri
+from typing import Literal, Optional, SupportsIndex
 
 from data import Data
 from player import DefaultDict, Player
-from tools import limit_range
+
+
+def limit_range(
+    value: float,
+    operator: Literal["+", "-", "*", "/"],
+    ratio: float,
+    min_value: float = 0.01,
+    max_value: float = 1,
+    n_digits: Optional[SupportsIndex] = 2,
+) -> float | int:
+    """
+    Limit the value to a specified range and round it to a specified number of decimal places.
+
+    Args:
+        value (float): The value to limit.
+        operator (str): The operator to use for limiting the value. Can be "+", "-", "*", or "/".
+        ratio (float): The ratio to apply to the value.
+        min_value (float): The minimum value of the range.
+        max_value (float): The maximum value of the range.
+        n_digits (int): The number of decimal places to round to.
+
+    Returns:
+        float: The limited and rounded value.
+    """
+    match operator:
+        case "+":
+            return round(max(min(value + ratio, max_value), min_value), n_digits)
+        case "-":
+            return round(max(min(value - ratio, max_value), min_value), n_digits)
+        case "*":
+            return round(max(min(value * ratio, max_value), min_value), n_digits)
+        case "/":
+            if ratio == 0:
+                return round(max(min(value, max_value), min_value), n_digits)
+            else:
+                return round(max(min(value / ratio, max_value), min_value), n_digits)
 
 
 def plant(player: Player, choices_farmland_id: tuple[int, ...], choice_seed_id: str) -> None:
@@ -99,5 +135,7 @@ def next_day(player: Player, data: Data) -> None:
                 print(data.text["class_0"].format(data.text[i.crop]))
             if i.growth_time >= data.crops[i.crop].growth_time:
                 i.ripe = True
+        elif i.soil_fertility > 0:
+            i.soil_fertility -= 1
     for i in player.corral:
         pass
